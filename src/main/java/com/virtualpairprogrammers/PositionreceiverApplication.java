8d2c3e2c1ff4c45d0125ab5e818bd7faa14e746b
Leela-Prasad <leela.jagu@gmail.com>
2019-04-20 21:37:12 +0530

    Update PositionreceiverApplication.java

--- src/main/java/com/virtualpairprogrammers/PositionreceiverApplication.java
+++ src/main/java/com/virtualpairprogrammers/PositionreceiverApplication.java
@@ -11,4 +11,17 @@ public class PositionreceiverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PositionreceiverApplication.class, args);
 	}
+	
+	@Bean
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) 
+	{
+	   EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils);
+	   AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+	   instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+	   instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+	   instance.setDataCenterInfo(info);
+	   instance.setNonSecurePort(8080);
+	   return instance;
+	}
+	
 }
