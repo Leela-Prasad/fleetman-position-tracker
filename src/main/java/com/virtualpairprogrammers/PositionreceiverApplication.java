2e1f1f11ac8c2f99770c0bd94d08f7f9a485328c
Leela-Prasad <leela.jagu@gmail.com>
2019-04-20 21:41:25 +0530

    Update PositionreceiverApplication.java

--- src/main/java/com/virtualpairprogrammers/PositionreceiverApplication.java
+++ src/main/java/com/virtualpairprogrammers/PositionreceiverApplication.java
@@ -4,6 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.netflix.appinfo.AmazonInfo;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PositionreceiverApplication {
